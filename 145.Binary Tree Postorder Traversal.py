70cd12374c747b820f4a9be4a70ea8e0a13765af
Administrator <admin@example.com>
2025-08-30 16:10:28 +0800

    docs: add 145.Binary Tree Postorder Traversal.py

---
+++ 145.Binary Tree Postorder Traversal.py
@@ -0,0 +1,52 @@
+#
+# @lc app=leetcode id=145 lang=python
+# @lcpr version=30202
+#
+# [145] Binary Tree Postorder Traversal
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def postorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        result = []
+        if root is None:
+            return result
+        
+        result.extend(self.postorderTraversal(root.left))
+        result.extend(self.postorderTraversal(root.right))
+        result.append(root.val)
+        return result
+        
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# [1,null,2,3]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [1,2,3,4,5,null,8,null,null,6,7,9]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# []\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [1]\n
+# @lcpr case=end
+
+#
+
--- README.md
+++ README.md
@@ -14,6 +14,7 @@ This repository contains solutions to LeetCode problems. Each solution is implem
 - `1.Two Sum.py`: Solution for LeetCode Problem 1 - Two Sum.
 - `144.Binary Tree Preorder Traversal.py`: Solution for LeetCode Problem 144 - Binary Tree Preorder Traversal.
 - `94.Binary Tree Inorder Traversal.py`: Solution for LeetCode Problem 94.Binary Tree Inorder Traversal.
+- `145.Binary Tree Postorder Traversal.py`: Solution for LeetCode Problem 145.Binary Tree Postorder Traversal.
 
 ## Usage
 
