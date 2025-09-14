90a68635763f525aae6a474256b3704f3b73158d
mile <re58g65412723@gmail.com>
2025-09-14 15:09:15 +0800

    docs:  add traverse solution in 92.

--- 92.Reverse Linked List II.py
+++ 92.Reverse Linked List II.py
@@ -14,37 +14,63 @@
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
         ''' Interative solution '''
+    #     # Step 1: determine traveral position
+    #     # 1.1 if left == 1, reverse from head.
+    #     if left == 1:
+    #         return self.reverseN(head, right)
+    #     # 1.2 if left > 1, find the predecessor of left
+    #     else:
+    #         # find the left's predecessor
+    #         pre = head
+    #         for i in range(1, left - 1):
+    #             pre = pre.next
+    #         # reverse the sublist starting from pre.next
+    #         pre.next = self.reverseN(pre.next, right - left + 1)
+    #         return head
+    
+    # def reverseN(self, head, n):
+    #     # Reverse the first n nodes of the linked list starting from head
+    #     if head is None or head.next is None:
+    #         return head
+    #     # Step 1: reverse the first n nodes
+    #     prev, curr, next = None, head, head.next
+    #     while n > 0:
+    #         curr.next = prev
+    #         prev = curr
+    #         curr = next
+    #         if next:
+    #             next = next.next
+    #         n -= 1
+    #     # Step 2: connect the reversed part with the rest of the list
+    #     head.next = curr
+    #     # now prev is the new head of the reversed sublist
+    #     return prev
+        ''' traverse solution '''
         # Step 1: determine traveral position
         # 1.1 if left == 1, reverse from head.
         if left == 1:
             return self.reverseN(head, right)
         # 1.2 if left > 1, find the predecessor of left
         else:
-            # find the left's predecessor
-            pre = head
-            for i in range(1, left - 1):
-                pre = pre.next
-            # reverse the sublist starting from pre.next
-            pre.next = self.reverseN(pre.next, right - left + 1)
+            head.next = self.reverseBetween(head.next, left - 1, right - 1)
             return head
-    
+        
+    # successor
+    successor = None
+    # reverse the first n nodes of the linked list starting from head
     def reverseN(self, head, n):
-        ''' Reverse the first n nodes of the linked list starting from head '''
-        if head is None or head.next is None:
+        if n == 1:
+            # record the (n+1)th node
+            self.successor = head.next
             return head
-        # Step 1: reverse the first n nodes
-        prev, curr, next = None, head, head.next
-        while n > 0:
-            curr.next = prev
-            prev = curr
-            curr = next
-            if next:
-                next = next.next
-            n -= 1
-        # Step 2: connect the reversed part with the rest of the list
-        head.next = curr
-        # now prev is the new head of the reversed sublist
-        return prev
+        # recursively reverse the first n-1 nodes
+        last = self.reverseN(head.next, n - 1)
+        # make head.next point to head
+        head.next.next = head
+        # connect with the successor
+        head.next = self.successor
+        return last
+    
         
 # @lc code=end
 
